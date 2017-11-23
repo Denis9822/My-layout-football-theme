5a3c22b4ff5aeb12fa36a3dd9a96ef92989bf42b
Denis <ss.set2013@yandex.ru>
2017-11-23 13:20:36 +0200

    NEW

---
+++ index (4).php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Футбол</title>
+	<link rel="stylesheet" type="text/css" href="style (3).css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Hammersmith+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa:700" rel="stylesheet">
+</head>
+<body>
+
+	<div class="wrapper"> 
+
+
+<div class="text"> </div>
+<div class="text1"> Football it's my life... </div>
+		
+<div class="rigth">	
+				<div class="a a1"> INFO </div>
+				<div class="a a2"> TEAM </div>
+				<div class="a a3"> PLAY </div>
+				<div class="a a4"> CUP </div>
+</div>
+
+
+</div>
+
+<div class="info">
+	
+<div class="about"> ABOUT FOOTBALL</div>
+<div class="about_content">Football is a family of team sports that involve, to varying degrees, kicking a ball with the foot to score a goal. Unqualified, the word football is understood to refer to whichever form of football is the most popular in the regional context in which the word appears. Sports commonly called 'football' in certain places include: association football (known as soccer in some countries); gridiron football (specifically American football or Canadian football); Australian rules football; rugby football (either rugby league or rugby union); and Gaelic football.[1][2] These different variations of football are known as football codes. <br/><br/>Various forms of football can be identified in history, often as popular peasant games. Contemporary codes of football can be traced back to the codification of these games at English public schools during the nineteenth century.[3][4] The expanse of the British Empire allowed these rules of football to spread to areas of British influence outside the directly controlled Empire.[5] By the end of the nineteenth century, distinct regional codes were already developing: Gaelic football, for example, deliberately incorporated the rules of local traditional football games in order to maintain their heritage.</div>
+</div>
+
+<div class="player">
+	
+	<div class="player_name"> PLAYER</div>
+
+	<div class="player_foto"></div>
+
+	<div class="player_content">
+		
+	<ul>
+		<li>Name: <span>Lionel Andrés Messi </span></li>
+		<li>Place:  <span>	Rosario, Argentina </span> </li>
+		<li>Playing position: <span>	Forward </span></li>
+		<li>Team:<span>	Barcelona </span> </li>
+		<li>Number: <span>	10 </span></li>
+
+	</ul>
+
+
+	</div>
+
+</div>
+
+</body>
+</html>
